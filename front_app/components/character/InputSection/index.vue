77d544b8ece9e773ca943c5e5e41ef2200ec2e0b
itanimura-wwwave <i-tanimura@wwwave.jp>
2025-06-17 09:58:18 +0900

    キャラクターを追加し、InputSectionコンポーネントでキャラ変更できるように設定

--- front_app/components/character/InputSection/index.vue
+++ front_app/components/character/InputSection/index.vue
@@ -6,7 +6,73 @@ import {
 import { useCreateAudioQuery } from '~/composables/useCreateAudioQueryRequest'
 import type { requestParams } from '~/api/CreateAudioQueryRequest'
 import type { ICreateAudioQueryResponse } from '~/core/voicevoxApiServer/responses/ICreateAudioQueryResponse'
-import { USAGI_PHRASES } from '~/data/phrases'
+import {
+  KASUKABE_PHRASES,
+  USAGI_PHRASES,
+  ZUNDAMON_PHRASES,
+} from '~/data/phrases'
+
+const USAGI_IDs = [61, 62, 63, 64]
+const ZUNDA_IDs = [1, 3, 5, 7, 22, 38, 75, 76]
+const KASUKABE_IDs = [8]
+
+const props = defineProps({
+  // v-model は 'modelValue' という名前のプロップスになる
+  modelValue: {
+    type: Number,
+    required: true,
+  },
+})
+const emit = defineEmits(['update:modelValue'])
+
+const currentCharacter = computed({
+  // 親の値を読み取る
+  get() {
+    return props.modelValue
+  },
+  // 子コンポーネント内で値が変更されたときに実行される
+  set(nextCharacter) {
+    // 'update:modelValue' イベントをemitして、親に変更を通知
+    emit('update:modelValue', nextCharacter)
+  },
+})
+
+let speakerId = 0
+let speakerText = '初期値です'
+const setSpeaker = () => {
+  switch (currentCharacter.value) {
+    case 0:
+      speakerId = USAGI_IDs[Math.floor(Math.random() * USAGI_IDs.length)]
+      speakerText =
+        USAGI_PHRASES[Math.floor(Math.random() * USAGI_PHRASES.length)]
+      return
+
+    case 1:
+      speakerText =
+        ZUNDAMON_PHRASES[Math.floor(Math.random() * ZUNDAMON_PHRASES.length)]
+      speakerId = ZUNDA_IDs[Math.floor(Math.random() * ZUNDA_IDs.length)]
+      return
+    case 2:
+      speakerId = KASUKABE_IDs[0]
+      speakerText =
+        KASUKABE_PHRASES[Math.floor(Math.random() * KASUKABE_PHRASES.length)]
+      return
+    default:
+      speakerId = 0
+  }
+}
+
+const changeSelectCharacter = (isMoveNext: boolean) => {
+  const CHARACTER_COUNT = 3
+  // 加算処理: 3になった場合0へループさせる
+  if (isMoveNext) {
+    currentCharacter.value = (currentCharacter.value + 1) % CHARACTER_COUNT
+    return
+  }
+  // 減算処理: 0だった場合2へループさせる
+  currentCharacter.value =
+    (currentCharacter.value - 1 + CHARACTER_COUNT) % CHARACTER_COUNT
+}
 
 const {
   isLoading: isLoadingSynthesis,
@@ -25,12 +91,13 @@ const {
 const audioPlayerElement = ref<HTMLAudioElement | null>(null)
 
 const callGenarateAudioQuery = () => {
-  const useText: string =
-    USAGI_PHRASES[Math.floor(Math.random() * USAGI_PHRASES.length)]
+  // 誰が、どんなテキストを喋るのかを設定する
+  setSpeaker()
+
   const requestParams: requestParams = {
     queryParams: {
-      text: useText,
-      speaker: 62,
+      text: speakerText,
+      speaker: speakerId,
     },
   }
   generateAudioQuery(requestParams)
@@ -42,7 +109,7 @@ const callGenarateAudioFile = (
   if (audioQuery) {
     const requestParams: allCamelCaseRequestParams = {
       requestBody: audioQuery,
-      queryParams: { speaker: 63 },
+      queryParams: { speaker: speakerId },
     }
     generateAudioFile(requestParams)
   }
@@ -68,7 +135,6 @@ watch(audioSrc, async (newSrc) => {
     await nextTick()
 
     // 3. audio要素の.play()メソッドを呼び出して再生を開始
-    // `?.` (オプショナルチェーン) を使って、要素がnullでない場合のみ実行する
     audioPlayerElement.value?.play()
   }
 })
@@ -92,9 +158,19 @@ watch(audioSrc, async (newSrc) => {
 
     <div class="w-full h-full flex items-center bg-blue-200">
       <div class="w-full flex justify-around">
-        <button class="border-2 border-black rounded-full">←</button>
-        <div class="flex items-center">□</div>
-        <button class="border-2 border-black rounded-full">→</button>
+        <button
+          class="border-2 border-black rounded-full"
+          @click="changeSelectCharacter(false)"
+        >
+          ←
+        </button>
+        <div class="flex items-center">{{ currentCharacter }}</div>
+        <button
+          class="border-2 border-black rounded-full"
+          @click="changeSelectCharacter(true)"
+        >
+          →
+        </button>
       </div>
     </div>
   </div>
--- front_app/components/character/index.vue
+++ front_app/components/character/index.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+const currentCharacter = ref<0 | 1 | 2>(0)
+</script>
+
 <template>
   <div class="flex items-center justify-center">
-    <CharacterInputSection class="w-1/2 h-full" />
-    <CharacterPictureSection class="w-1/2 h-full" />
+    <CharacterInputSection v-model="currentCharacter" class="w-1/2 h-full" />
+    <CharacterPictureSection v-model="currentCharacter" class="w-1/2 h-full" />
   </div>
 </template>
